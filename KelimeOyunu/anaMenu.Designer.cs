093b714417a8206bd37d22357174de39dca05ee8
barisgokdemir <44324850+barisgokdemir@users.noreply.github.com>
2021-04-16 21:46:18 +0300

    Add files via upload

---
+++ KelimeOyunu/anaMenu.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace KelimeOyunu
+{
+    partial class anaMenu
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.oyuncuText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(61)))), ((int)(((byte)(99)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyuncu Adı";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(158, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(427, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Yeni Oyun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(158, 167);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(427, 32);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Soru Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // oyuncuText
+            // 
+            this.oyuncuText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.oyuncuText.Location = new System.Drawing.Point(158, 91);
+            this.oyuncuText.Name = "oyuncuText";
+            this.oyuncuText.Size = new System.Drawing.Size(427, 29);
+            this.oyuncuText.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(158, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(427, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Skor Tablosu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // anaMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::KelimeOyunu.Properties.Resources.arkaplan5;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.oyuncuText);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "anaMenu";
+            this.Text = "Kelime Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox oyuncuText;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ KelimeOyunu/json.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace KelimeOyunu
+{
+    class json
+    {
+        public List<kelime> JsonOkuma(string dosyayolu)
+        {
+            string JsonOkunanData = File.ReadAllText(dosyayolu, Encoding.GetEncoding("iso-8859-9"));
+            List<kelime> okunanJson = JsonConvert.DeserializeObject<List<kelime>>(JsonOkunanData);
+            List<kelime> istenenKelime = new List<kelime>();
+
+            //MessageBox.Show(okunanJson[1].soru);
+        
+            foreach (var item in okunanJson)
+            {
+                
+                    istenenKelime.Add(item);
+                
+
+                //MessageBox.Show(item.id);
+            }
+            return istenenKelime;
+            
+        }
+        public void JsonYazma(string ekleSoru, string ekleCevap, string ekleHSayi,string dosyayolu)
+        {
+            kelime yenikelime = new kelime();
+            yenikelime.soru = ekleSoru;
+            yenikelime.cevap = ekleCevap;
+            yenikelime.harfSayisi = ekleHSayi;
+            
+            //json dosyasından tüm veriyi çek
+            string JsonOkunanData = File.ReadAllText(dosyayolu, Encoding.GetEncoding("iso-8859-9"));
+            //veriyi listeye döüştür
+            List<kelime> okunanJson = Newtonsoft.Json.JsonConvert.DeserializeObject<List<kelime>>(JsonOkunanData);
+            //listeye yeni veriyi ekle 
+            okunanJson.Add(yenikelime);
+            //listeyi jsona dönüştür
+            string jsonkelime = Newtonsoft.Json.JsonConvert.SerializeObject(okunanJson,Formatting.Indented);
+            //jsona yaz 
+           File.WriteAllText(@"C:\Users\baris\source\repos\KelimeOyunu\sorucevap.json", jsonkelime, Encoding.GetEncoding("iso-8859-9"));
+            MessageBox.Show("yazdi");
+        }
+        public void JsonYazmaSkor(string oyuncuAdi , string kalanSure, string oyunanmaZamani, string puan, string basariDurumu, string dosyayolu)
+        {
+            Skor newSkor = new Skor();
+            newSkor.oyuncuAdi = oyuncuAdi;
+            newSkor.kalanSure = kalanSure;
+            newSkor.oyunanmaZamani = oyunanmaZamani;
+            newSkor.puan = puan;
+            newSkor.basariDurumu = basariDurumu;
+            //json dosyasından tüm veriyi çek
+            string JsonOkunanData = File.ReadAllText(dosyayolu, Encoding.GetEncoding("iso-8859-9"));
+            //veriyi listeye döüştür
+            List<Skor> okunanJson = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Skor>>(JsonOkunanData);
+            //listeye yeni veriyi ekle 
+            okunanJson.Add(newSkor);
+            //listeyi jsona dönüştür
+            string jsonkelime = Newtonsoft.Json.JsonConvert.SerializeObject(okunanJson, Formatting.Indented);
+            //jsona yaz 
+            File.WriteAllText(@"C:\Users\baris\source\repos\KelimeOyunu\Skor.json", jsonkelime, Encoding.GetEncoding("iso-8859-9"));
+            MessageBox.Show("yazdi");
+        }
+        public List<Skor> JsonOkumaSkor(string dosyayolu)
+        {
+            string JsonOkunanData = File.ReadAllText(dosyayolu, Encoding.GetEncoding("iso-8859-9"));
+            List<Skor> okunanJson = JsonConvert.DeserializeObject<List<Skor>>(JsonOkunanData);
+            List<Skor> istenenKelime = new List<Skor>();
+
+            //MessageBox.Show(okunanJson[1].soru);
+
+            foreach (var item in okunanJson)
+            {
+
+                istenenKelime.Add(item);
+
+
+                //MessageBox.Show(item.id);
+            }
+            return istenenKelime;
+
+        }
+
+    }
+}
